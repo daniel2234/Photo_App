838209d4de6aabd7361967c1ab835a1aa5bd833b
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-06-22 17:12:58 -0400

    added an edit feature

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  
   def index
     @pictures = Picture.all
   end
@@ -14,10 +15,27 @@ class PicturesController < ApplicationController
 
   def create
     @picture = Picture.new(picture_params)
+    # make a new picture with what picture_params returns (which is a method we're calling)
     if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
       redirect_to pictures_url
     else
       render :new
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{picture.id}"
+    else
+      # otherwise render the view associated with the action :edit (i.e. edit.html.erb)
+      render :edit
     end
   end
 
